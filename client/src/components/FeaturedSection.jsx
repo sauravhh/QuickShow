48fdaad47987ff4c65361ba7c6eecd82b57ae0da
sauravhh <SauravTamang@kbc.edu.np>
2026-03-14 19:29:53 +0545

    add trailer section and footer

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -21,7 +21,7 @@ const FeaturedSection = () => {
             </div>
 
             <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
-                {dummyShowsData.slice(0, 4).map((show)=> (
+                {dummyShowsData.slice(0, 5).map((show)=> (
                     <MovieCard key={show._id} movie={show}/>
                 ))}
             </div>
